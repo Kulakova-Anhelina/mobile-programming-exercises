e4812134f3afb5973ef051c332617aaa456c06f6
Kulakova-Anhelina <kulakovaanhelina@gmail.com>
2020-03-17 20:34:33 +0200

    expanding exercise

--- ex10.js
+++ ex10.js
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Alert, StyleSheet, View, Button, TextInput } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Callout } from "react-native-maps";
 
 export default function App() {
   const [region, setRegion] = useState({
     lat: 60.200692,
-    long: 24.934302,
-    error: null,
-    name: ""
+    long: 24.934302
   });
 
   const [location, setLocation] = useState("");
-  const appiKey = "AIzaSyB9FJAiFsG2_uVzvSn1bmf-yaP6VK5f_L8";
+  const appiKey = "AIzaSyAMpPEnw1dpxC6kMv14xkLnaZatRH_9seY";
+  const [data, setData] = useState([]);
 
-  const getLocation = () => {
-    const url =
-      "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+" +
-      location +
-      "&key=" +
-      appiKey;
-    fetch(url)
+const getUrl = (lat, long) =>{
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&region=fi&key=${appiKey}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => {
+      setRegion({
+        lat: responseJson.results[0].geometry.location.lat,
+        long: responseJson.results[0].geometry.location.lng
+      });
+    });
+
+    return lat = region.lat, long = region.long
+}
+
+  
+
+  const getPlace = () => {
+ let myLat = getUrl();
+  setRegion({
+    lat: myLat.lat,
+    long: myLat.long
+
+  })
+    const url2 = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${region.lat},${region.long}&radius=1500&type=restaurant&&key=${appiKey}`;
+    fetch(url2)
       .then(response => response.json())
       .then(responseJson => {
-        setRegion({
-          lat: responseJson.results[0].geometry.location.lat,
-          long: responseJson.results[0].geometry.location.lng,
-          name: responseJson.results[0].name
-        });
+        setData(responseJson.results);
       })
       .catch(error => {
         Alert.alert("Error", error);
@@ -37,19 +50,25 @@ export default function App() {
     <View style={styles.container}>
       <MapView
         style={{ height: "100%", flex: 2 }}
-        region={{
+        initialRegion={{
           latitude: region.lat,
           longitude: region.long,
           latitudeDelta: 0.0322,
           longitudeDelta: 0.0221
         }}
       >
-        <Marker
-          coordinate={{ latitude: region.lat, longitude: region.long }}
-          title={region.name}
-        />
+        <Callout>
+          {data.map(marker => (
+            <Marker
+              coordinate={{
+                latitude: marker.geometry.location.lat,
+                longitude: marker.geometry.location.lng
+              }}
+              title={marker.name}
+            />
+          ))}
+        </Callout>
       </MapView>
-
       <TextInput
         style={{ fontSize: 18 }}
         value={location}
@@ -57,7 +76,7 @@ export default function App() {
         onChangeText={location => setLocation(location)}
       />
 
-      <Button title="Show" onPress={getLocation} />
+      <Button title="Show" onPress={getPlace} />
     </View>
   );
 }
